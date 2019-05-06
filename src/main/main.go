ead5189bb08dedff5cbfa22243bebdb3800d281c
xwolf <2324808561@qq.com>
2019-05-06 14:11:29 +0800

    561. Array Partition I

--- src/main/main.go
+++ src/main/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"books/diaryoflearngo/ch03"
+	"algorithm/leetcode"
 )
 
 func main() {
 
-	ch03.FuncTest6(10)
-
+	//ch03.FuncTest6(10)
+	nums := []int{4, 1, 5, 6}
+	leetcode.ArrayPairSum(nums)
 }
