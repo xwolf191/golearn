b928113b0c31935a2c32aa3ceaf65f3f3f2be017
xwolf191 <snowxwolf@foxmail.com>
2019-12-05 19:48:01 +0800

    main

--- src/books/diaryoflearngo/ch01/basic.go
+++ src/books/diaryoflearngo/ch01/basic.go
@@ -4,22 +4,22 @@ import (
 	"fmt"
 )
 
- /**
-  * go基本变量声明、初始化
-  */
-func Basic(){
+/**
+ * go基本变量声明、初始化
+ */
+func Basic() {
 	fmt.Println("Hello Go Basic")
 	var a int = 1
-	fmt.Printf("a=%d\n",a)
+	fmt.Printf("a=%d\n", a)
 	//初始化多个变量
-	var b,c = 3,3
-	fmt.Printf("%d+%d=%d\n",b,c,b+c)
+	var b, c = 3, 3
+	fmt.Printf("%d+%d=%d\n", b, c, b+c)
 	//默认值0
 	var e int
-	fmt.Printf("e=%d\n",e)
+	fmt.Printf("e=%d\n", e)
 	//变量声明及初始化
 	f := "var"
-	fmt.Printf("f=%s\n",f)
+	fmt.Printf("f=%s\n", f)
 	//强制转换
 	var g byte = 123
 	println(g)
@@ -28,21 +28,22 @@ func Basic(){
 /**
  * 条件判断
  */
-func Condition(age int)  {
+func Condition(age int) {
 
-	if age<10 {
+	if age < 10 {
 		fmt.Println("10<age<40")
-	}else if age < 40 && age >10 {
+	} else if age < 40 && age > 10 {
 		fmt.Println("10<= age <=40")
 	} else {
-		 fmt.Println("a>40")
+		fmt.Println("a>40")
 	}
 }
+
 /**
   switch  default
- */
-func Switch(a int)  {
-    switch(a){
+*/
+func Switch(a int) {
+	switch a {
 	case 1:
 		fmt.Println("a=1")
 	case 2:
@@ -58,12 +59,12 @@ func Switch(a int)  {
 /**
  *  循环
  */
-func Cycle()  {
+func Cycle() {
 	//定义常量
-	const LENGTH  = 5
+	const LENGTH = 5
 	//定义数组
-	var a =[LENGTH]int{3,43,3,5}
-	for i:=0;i<LENGTH;i++ {
-		fmt.Printf("a[%d]=%d\n",i,a[i])
+	var a = [LENGTH]int{3, 43, 3, 5}
+	for i := 0; i < LENGTH; i++ {
+		fmt.Printf("a[%d]=%d\n", i, a[i])
 	}
 }
